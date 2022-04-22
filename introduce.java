a0c2e1d1f47efcc975d6721432ad2063fbb16951
OgatsuMayuko <95006358+OgatsuMayuko@users.noreply.github.com>
2022-04-22 15:31:16 +0900

    Add files via upload

---
+++ introduce.java
@@ -0,0 +1,7 @@
+public class introduce{
+  public static void main(String[] args){
+    System.out.println("私の好きな食べ物はお寿司です。");
+    System.out.println("特に好きなネタはサーモンといくらで、サーモンいくら丼が大好きです。");
+    System.out.println("理由は、ジャンクフードや揚げ物などのように体に悪くないため、食べても罪悪感がないからです。");
+  }
+}
